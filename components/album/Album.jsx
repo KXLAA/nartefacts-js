425e34e2945ffdb364c88e08a2ce42d7838e3a61
Kolade Afode <kolade.afode@yahoo.com>
2021-12-11 00:12:49 +0100

    Align items in Album Page

--- components/album/Album.jsx
+++ components/album/Album.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { BsSuitHeartFill } from 'react-icons/bs';
 
 const Container = styled.section`
   border: 8px solid black;
@@ -30,15 +31,57 @@ const ColorBox = styled.div`
   background-color: ${(props) => props.bg};
 `;
 
-const DetailsContainer = styled.div`
+const Info = styled.div`
   display: flex;
   padding: 16px;
   gap: 16px;
+  padding-top: 24px;
+  padding-bottom: 24px;
+  align-items: center;
 `;
 
-const Detail = styled.div``;
+const Detail = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  justify-content: space-between;
+
+  h1 {
+    font-weight: 900;
+    font-size: 96px;
+    line-height: 128px;
+  }
+
+  h2 {
+    font-weight: 900;
+    font-size: 48px;
+    line-height: 64px;
+  }
+`;
+
+const AlbumDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Like = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  font-weight: 900;
+  font-size: 96px;
+  line-height: 128px;
+`;
+
+const ArtistImage = styled.img`
+  width: 150px;
+  height: 150px;
+  border-radius: 100%;
+  border: 8px solid #000000;
+`;
 
 const Album = ({ album }) => {
+  console.log(album);
   return (
     <Container>
       <AlbumContainer>
@@ -49,12 +92,22 @@ const Album = ({ album }) => {
           ))}
         </ColorPalette>
       </AlbumContainer>
-      <DetailsContainer>
+
+      <Info>
+        <ArtistImage src={album.artist.photoURL} alt="Picture of the author" />
+
         <Detail>
-          <h1>Heloo</h1>
-          <h1>Heloo</h1>
+          <AlbumDetails>
+            <h1>{album.artist.name}</h1>
+            <h2>{album.title} </h2>
+          </AlbumDetails>
+
+          <Like>
+            <BsSuitHeartFill tyle={{ fontSize: '32px' }} />
+            <p>{album.likeCount}</p>
+          </Like>
         </Detail>
-      </DetailsContainer>
+      </Info>
     </Container>
   );
 };
